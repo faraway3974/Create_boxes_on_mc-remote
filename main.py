0c2bc5f3c3fed0643c345389d8b34e621c1029de
faraway3974 <174330666+faraway3974@users.noreply.github.com>
2026-04-29 20:16:52 +0900

    Update

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from mc_remote.minecraft import Minecraft
 import param_mc_remote as param
+from param_mc_remote import PLAYER_ORIGIN as PO
 from param_mc_remote import block
 from createbox import CreateBox
 
-Box1 = CreateBox(10, 63, 10, 3, 3, 3, block.DIAMOND_BLOCK)
-Box1.create_box()
+mc = Minecraft.create(address=param.ADRS_MCR, port=param.PORT_MCR)
+mc.setPlayer(param.PLAYER_NAME, PO.x, PO.y, PO.z)
+
+Box1 = CreateBox()
+Box1.create_box(mc)
