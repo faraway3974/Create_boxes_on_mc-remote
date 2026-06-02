9c0c8c74fd215f9580d327554403a1b6e9a0af6d
faraway3974 <174330666+faraway3974@users.noreply.github.com>
2026-06-02 22:39:05 +0900

    Change file name

--- sample_code.py
+++ sample_code.py
@@ -2,7 +2,7 @@ from mc_remote.minecraft import Minecraft
 import param_mc_remote as param
 from param_mc_remote import PLAYER_ORIGIN as PO
 from param_mc_remote import block
-from createbox import create_box
+from create_box import create_box
 
 mc = Minecraft.create(address=param.ADRS_MCR, port=param.PORT_MCR)
 mc.setPlayer(param.PLAYER_NAME, PO.x, PO.y, PO.z)
